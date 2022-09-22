7f73eefc843b047e7e2dab0b9be7d18f52c6f406
Chris Allmark <chris.allmark@gmail.com>
2022-09-22 14:20:07 +0100

    Refactor for multiple displays

--- src/index.ts
+++ src/index.ts
@@ -44,66 +44,75 @@ const btt: BTTPreset = defaults;
 btt.BTTPresetName = "Generated";
 btt.BTTPresetUUID = uuid();
 
-const FACTOR = 6;
+const FACTOR = 2;
+const MENU_BAR = 42;
 
-const resolution = btt.BTTPresetSnapAreas[0].BTTDisplayResolution.match(/\d+/g);
-if (!resolution || resolution.length !== 2) {
-  throw new Error("Invalid resolution");
-}
-const screenWidth = parseInt(resolution[0]);
-const screenHeight = parseInt(resolution[1]);
-
-const generateSnapAreas = (columns: number, rows: number): BTTSnapArea[] => {
-  const cellWidth = Math.floor(screenWidth / columns);
-  const cellHeight = Math.floor(screenHeight / rows);
-  const offsetX = Math.floor(
-    (screenWidth - Math.floor((cellWidth / FACTOR) * columns)) / 2
-  );
-  const offsetY = Math.floor(
-    (screenHeight - Math.floor((cellHeight / FACTOR) * rows)) / 2
-  );
-
+const generateSnapAreas = (bttDisplay: BTTDisplay): BTTSnapArea[] => {
+  const resolution = bttDisplay.BTTDisplayResolution.match(/\d+/g);
+  if (!resolution || resolution.length !== 2) {
+    throw new Error("Invalid resolution");
+  }
+  const screenWidth = parseInt(resolution[0]);
+  const screenHeight = parseInt(resolution[1]) - MENU_BAR;
   const bttSnapAreas: BTTSnapArea[] = [];
-  for (let column = 0; column < columns; column += 1) {
-    for (let row = 0; row < rows; row += 1) {
-      const bttSnapArea: BTTSnapArea = {
-        ...btt.BTTPresetSnapAreas[0].BTTDisplayDisplayedDragpoints[0],
-        BTTSnapAreaPreviewFrame: `{{${column * cellWidth}, ${
-          row * cellHeight
-        }}, {${cellWidth}, ${cellHeight}}}`,
-        BTTSnapAreaFrame: `{{${
-          offsetX + column * Math.floor(cellWidth / FACTOR)
-        }, ${offsetY + row * Math.floor(cellHeight / FACTOR)}}, {${Math.floor(
-          cellWidth / FACTOR
-        )}, ${Math.floor(cellHeight / FACTOR)}}}`,
-        BTTSnapAreaUUID: uuid().toUpperCase(),
-      };
-      switch (columns) {
-        case 3:
-          bttSnapArea.BTTSnapAreaModifierCTRL = 1;
-          break;
-        case 4:
-          bttSnapArea.BTTSnapAreaModifierOPT = 1;
-          break;
-        case 5:
-          bttSnapArea.BTTSnapAreaModifierCMD = 1;
-          break;
+  for (let columns = 3; columns <= 5; columns++) {
+    for (let rows = 2; rows <= 3; rows++) {
+      const cellWidth = Math.floor(screenWidth / columns);
+      const cellHeight = Math.floor(screenHeight / rows);
+      const offsetX = Math.floor(
+        (screenWidth - Math.floor((cellWidth / FACTOR) * columns)) / 2
+      );
+      const offsetY =
+        MENU_BAR +
+        Math.floor(
+          (screenHeight - Math.floor((cellHeight / FACTOR) * rows)) / 2
+        );
+      for (let column = 0; column < columns; column += 1) {
+        for (let row = 0; row < rows; row += 1) {
+          const bttSnapArea: BTTSnapArea = {
+            ...bttDisplay.BTTDisplayDisplayedDragpoints[0],
+            BTTSnapAreaPreviewFrame: `{{${column * cellWidth}, ${
+              row * cellHeight
+            }}, {${cellWidth}, ${cellHeight}}}`,
+            BTTSnapAreaFrame: `{{${
+              offsetX + column * Math.floor(cellWidth / FACTOR)
+            }, ${
+              offsetY + row * Math.floor(cellHeight / FACTOR)
+            }}, {${Math.floor(cellWidth / FACTOR)}, ${Math.floor(
+              cellHeight / FACTOR
+            )}}}`,
+            BTTSnapAreaUUID: uuid().toUpperCase(),
+          };
+          switch (columns) {
+            case 3:
+              bttSnapArea.BTTSnapAreaModifierCTRL = 1;
+              break;
+            case 4:
+              bttSnapArea.BTTSnapAreaModifierOPT = 1;
+              break;
+            case 5:
+              bttSnapArea.BTTSnapAreaModifierCMD = 1;
+              break;
+          }
+          if (rows === 3) {
+            bttSnapArea.BTTSnapAreaModifierSHIFT = 1;
+          }
+          bttSnapAreas.push(bttSnapArea);
+        }
       }
-      if (rows === 3) {
-        bttSnapArea.BTTSnapAreaModifierSHIFT = 1;
-      }
-      bttSnapAreas.push(bttSnapArea);
     }
   }
   return bttSnapAreas;
 };
 
-let snapAreas: BTTSnapArea[] = [];
-for (let i = 3; i <= 5; i++) {
-  snapAreas = snapAreas.concat(
-    generateSnapAreas(i, 2),
-    generateSnapAreas(i, 3)
-  );
+let bttPresetSnapAreas = [];
+for (const bttDisplay of btt.BTTPresetSnapAreas) {
+  const snapAreas = generateSnapAreas(bttDisplay);
+  bttPresetSnapAreas.push({
+    ...bttDisplay,
+    BTTDisplayDisplayedDragpoints: snapAreas,
+    BTTDisplaySnappedDragpoints: snapAreas,
+  });
 }
 
 fs.writeFileSync(
@@ -111,13 +120,7 @@ fs.writeFileSync(
   JSON.stringify(
     {
       ...btt,
-      BTTPresetSnapAreas: [
-        {
-          ...btt.BTTPresetSnapAreas[0],
-          BTTDisplayDisplayedDragpoints: snapAreas,
-          BTTDisplaySnappedDragpoints: snapAreas,
-        },
-      ],
+      BTTPresetSnapAreas: bttPresetSnapAreas,
     },
     null,
     2
